17728f7c97d8294d67ca0d7f02dbab615734fbd0
Jiaqi <76413247+jiaqiwang969@users.noreply.github.com>
2022-11-12 09:13:21 +0800

    fix a bug
            f=cumsum(x^(beta/Cp)*(Tr/x+Omag*x)^2/x);
           (f-f(1))
    
    which is equal to int_[1,ratio], not int_[ratio,1]

--- subfunction/entropyPara.m
+++ subfunction/entropyPara.m
@@ -1,24 +1,27 @@
 function [c02,rou0,P0,s0]=entropyPara(r,N,Ratio,Omag,Tr,Entropy,beta)     
 Cp=1.005;Cv=0.718;gama=1.4;
 switch  Entropy
-    case {0} %Isentropic
+    case {0} %å‡ç†µ
         c02=1-1/2*(gama-1)*(Omag^2*(1-r.^2)-Tr^2*(1-1./(r.^2)))+2*(gama-1)*Omag*Tr*log(r);
-        rou0=c02.^(1/(gama-1)); P0=1/gama*(c02.^(gama/(gama-1)));%Isentropic(·Ç¾ùìØ¶àÒ»¸ö²ÎÊıbeita-3.36)
+        rou0=c02.^(1/(gama-1)); P0=1/gama*(c02.^(gama/(gama-1)));%å‡ç†µå‡è®¾(éå‡ç†µå¤šä¸€ä¸ªå‚æ•°beita-3.36)
              
     case {1}  %by Guan 
         EntropyFun=chebop(Ratio,1);EntropyFun.rbc=1;EntropyFun.op=@(x,rou0) diff(rou0)-(beta/gama)./x*rou0-(Tr./x+Omag*x)^2*x^(beta-1)*rou0^(2-gama);
         rou0=EntropyFun\0;rou0=rou0(r);
-        P0=1/gama*(rou0.^gama)./(r.^beta);   %no-Isentropic-3.36
+        P0=1/gama*(rou0.^gama)./(r.^beta);   %éå‡ç†µå‡è®¾-3.36
         c02=gama*P0./rou0;
     case {2}  %by James   
         x = chebfun('x',[Ratio 1]);
-        c02 = x^(-beta/Cp)-(gama-1)*x^(-beta/Cp)*cumsum(x^(beta/Cp)*(Tr/x+Omag*x)^2/x);c02=c02(r);%(James-2.1.19)
+        f=cumsum(x^(beta/Cp)*(Tr/x+Omag*x)^2/x);
+        c02 = 1*x^(-beta/Cp)-(gama-1)*x^(-beta/Cp)*(f-f(1));c02=c02(r);%(James-2.1.19)
         rou0=(c02.*r.^(beta/Cv)).^(1/(gama-1));%(James-2.1.20)
         P0=(rou0.^gama)./gama.*exp(-log(r.^beta)./Cv); %(James-2.1.12) s0=-log(r^beta)
-    case {3}  %for James  ½«gama->1 from Guan ²Î¿¼ìØÎª0=ln£¨1£©
+
+
+    case {3}  %for James  å°†gama->1 from Guan å‚è€ƒç†µä¸º0=lnï¼ˆ1ï¼‰
         EntropyFun=chebop(Ratio,1);EntropyFun.rbc=1;EntropyFun.op=@(x,rou0) diff(rou0)-(beta/gama)./x*rou0-1/gama*(Tr./x+Omag*x)^2*x^(beta-1)*rou0^(2-gama);%add 1/gama
         rou0=EntropyFun\0;rou0=rou0(r);
-        P0=(rou0.^gama)./(r.^beta);   %no-Isentropic-3.36
+        P0=(rou0.^gama)./(r.^beta);   %éå‡ç†µå‡è®¾-3.36
         c02=gama*P0./rou0;
     case {4}  %for Guan2 same as Tam --3.87  rou0=1
         rou0=1*ones(N+1,1);
